755843f45a1ae56a3e633c430d73396a669de913
mjbrow17 <mike.j.brown@outlook.com>
2023-03-19 16:02:32 -0700

    20230319 - Start

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 //routes
-const studentRoute = require('./routes/studentRoute');
-app.use('/student', studentRoute);
+const loanRoute = require('./routes/loanRoute');
+app.use('/loan', loanRoute);
 
-const gradeRoute = require('./routes/gradeRoute');
-app.use('/grade', gradeRoute);
+const customerRoute = require('./routes/customerRoute');
+app.use('/customer', customerRoute);
 
-
+const ledgerRoute = require('./routes/ledgerRoute');
+app.use('/ledger', ledgerRoute);
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
@@ -26,6 +27,6 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 //Connect to DB
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://mikejbrown:ssOX5PclZpKgf2VE@cluster0.dk5ltnv.mongodb.net/test', {useNewUrlParser: true})
-    .then(() => console.log('connection successfull'))
+mongoose.connect('mongodb+srv://mikejbrown:ssOX5PclZpKgf2VE@cluster0.dk5ltnv.mongodb.net/LOANS', {useNewUrlParser: true})
+    .then(() => console.log('connection successfull\n**********************************'))
     .catch((error) => console.error(err));
